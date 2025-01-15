99a11d72c1738a9c394637f3ef97019324c10028
kaissot <ramirez.kaiss@icloud.com>
2025-01-15 16:37:14 +0100

    error check and sa sb ss done

--- src/errorcheck.c
+++ src/errorcheck.c
@@ -6,7 +6,7 @@
 /*   By: karamire <karamire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/06 17:21:57 by karamire          #+#    #+#             */
-/*   Updated: 2025/01/15 15:51:14 by karamire         ###   ########.fr       */
+/*   Updated: 2025/01/15 16:37:06 by karamire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,61 +18,65 @@ int	syntax_error(char *av)
 	int	len;
 
 	i = 0;
-	if ((av[i] == '-' || av[i] == '+')  && ft_isdigit(av[i + 1]))
+	if ((av[i] == '-' || av[i] == '+') && ft_isdigit(av[i + 1]))
 		i++;
 	len = ft_strlen(av);
-	while(av[i] != '\0')
+	while (av[i] != '\0')
 	{
-		if(!ft_isdigit(av[i]))
-			return(0);
+		if (!ft_isdigit(av[i]))
+			return (0);
 		i++;
 	}
-	return(1);
+	return (1);
 }
 
 int	duplicate_error(t_list *stack_a, int nb)
 {
-	t_list *check;
+	t_list	*check;
 
 	check = stack_a;
-	while(check != NULL)
+	while (check != NULL)
 	{
 		if (check->value == nb)
-			return(-1);
+			return (-1);
 		check = check->next;
 	}
-	return(1);
+	return (1);
 }
 
-void print_stack(t_list *stack)
+void	print_stack(t_list *stack)
 {
-    t_list *current = stack;
-    while (current != NULL)
-    {
-        printf("%d -> ", current->value);
-        current = current->next;
-    }
+	t_list	*current;
+
+	current = stack;
+	while (current != NULL)
+	{
+		printf("%d -> ", current->value);
+		current = current->next;
+	}
+	printf("NULL");
 }
 
 void	check_stack(t_list **stack_a, int ac, char **av)
 {
-	int	i;
-	int	nbr;
-	t_list **stack_b = NULL;
+	int		i;
+	int		nbr;
+	t_list	**stack_b;
 
+	stack_b = NULL;
 	i = 0;
-	while(++i < ac)
+	while (++i < ac)
 	{
 		if (!syntax_error(av[i]))
 		{
 			write(1, "synta", 5);
-			return;
+			return ;
 		}
 		nbr = ft_atoi(av[i]);
 		if (duplicate_error(*stack_a, nbr) == -1)
 		{
 			write(1, "Error", 5);
-			return;
+			return ;
 		}
 		put_in_stack_a(stack_a, nbr, i);
 	}
