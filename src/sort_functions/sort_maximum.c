ec53462b8298d6681b6e9319ca66f23b59075717
kaissot <ramirez.kaiss@icloud.com>
2025-01-29 15:52:33 +0100

    remove subject

--- src/sort_functions/sort_maximum.c
+++ src/sort_functions/sort_maximum.c
@@ -6,7 +6,7 @@
 /*   By: karamire <karamire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/18 06:39:52 by kaissramire       #+#    #+#             */
-/*   Updated: 2025/01/28 21:11:18 by karamire         ###   ########.fr       */
+/*   Updated: 2025/01/29 15:47:29 by karamire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ int	search_bigger(t_list *stack_b)
 }
 void	first_push(t_list **stack_a, t_list **stack_b, int chunk)
 {
-	int		i;
+	int	i;
 
 	i = 0;
 	while ((*stack_a) != NULL)
