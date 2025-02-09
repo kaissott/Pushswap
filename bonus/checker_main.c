c160db42354eec54083c43614c01a7c8f0327f9b
kaissot <ramirez.kaiss@icloud.com>
2025-02-09 07:06:46 +0100

    checker done, some changes and almost finished the leak

--- bonus/checker_main.c
+++ bonus/checker_main.c
@@ -6,7 +6,7 @@
 /*   By: karamire <karamire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/09 00:45:20 by karamire          #+#    #+#             */
-/*   Updated: 2025/02/09 06:28:59 by karamire         ###   ########.fr       */
+/*   Updated: 2025/02/09 07:06:21 by karamire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,25 +24,25 @@ int	ft_strcmp(const char *s1, const char *s2)
 
 int	do_command(t_list **stack_a, t_list **stack_b, char *input)
 {
-	if (ft_strcmp("sa\n", input) == 0 && stack_a != NULL)
+	if (ft_strcmp("sa\n", input) == 0 && *stack_a != NULL)
 		sa(stack_a, 0);
-	else if (ft_strcmp("sb\n", input) == 0 && stack_b != NULL)
+	else if (ft_strcmp("sb\n", input) == 0 && *stack_b != NULL)
 		sb(stack_b, 0);
-	else if (ft_strcmp("ss\n", input) == 0 && stack_a != NULL)
+	else if (ft_strcmp("ss\n", input) == 0 && *stack_a != NULL)
 		ss(stack_a, stack_b, 0);
-	else if (ft_strcmp("ra\n", input) == 0 && stack_a != NULL)
+	else if (ft_strcmp("ra\n", input) == 0 && *stack_a != NULL)
 		ra(stack_a, 0);
-	else if (ft_strcmp("rb\n", input) == 0 && stack_b != NULL)
+	else if (ft_strcmp("rb\n", input) == 0 && *stack_b != NULL)
 		rb(stack_b, 0);
-	else if (ft_strcmp("rr\n", input) == 0 && stack_a != NULL)
+	else if (ft_strcmp("rr\n", input) == 0 && *stack_a != NULL)
 		rr(stack_a, stack_b, 0);
 	else if (ft_strcmp("pa\n", input) == 0)
 		pa(stack_a, stack_b, 0);
 	else if (ft_strcmp("pb\n", input) == 0)
 		pb(stack_a, stack_b, 0);
-	else if (ft_strcmp("rra\n", input) == 0 && stack_a != NULL)
+	else if (ft_strcmp("rra\n", input) == 0 && *stack_a != NULL)
 		rra(stack_a, 0);
-	else if (ft_strcmp("rrb\n", input) == 0 && stack_a != NULL)
+	else if (ft_strcmp("rrb\n", input) == 0 && *stack_a != NULL)
 		rrb(stack_b, 0);
 	else if (ft_strcmp("rrr\n", input) == 0)
 		rrr(stack_a, stack_b, 0);
@@ -52,12 +52,12 @@ int	do_command(t_list **stack_a, t_list **stack_b, char *input)
 }
 int	check_params(char *str)
 {
-	if (ft_strcmp("sa", str) == 0 || ft_strcmp("sb", str) == 0
-		|| ft_strcmp("ss", str) == 0 || ft_strcmp("pa", str) == 0
-		|| ft_strcmp("pb", str) == 0 || ft_strcmp("ra", str) == 0
-		|| ft_strcmp("rb", str) == 0 || ft_strcmp("rr", str) == 0
-		|| ft_strcmp("rra", str) == 0 || ft_strcmp("rrb", str) == 0
-		|| ft_strcmp("rrr", str) == 0)
+	if (ft_strcmp("sa\n", str) == 0 || ft_strcmp("sb\n", str) == 0
+		|| ft_strcmp("ss\n", str) == 0 || ft_strcmp("pa\n", str) == 0
+		|| ft_strcmp("pb\n", str) == 0 || ft_strcmp("ra\n", str) == 0
+		|| ft_strcmp("rb\n", str) == 0 || ft_strcmp("rr\n", str) == 0
+		|| ft_strcmp("rra\n", str) == 0 || ft_strcmp("rrb\n", str) == 0
+		|| ft_strcmp("rrr\n", str) == 0)
 		return (1);
 	else
 		return (-1);
@@ -83,12 +83,13 @@ int	check_input(t_list **stack_a, t_list **stack_b)
 	input = get_next_line(0);
 	while (input != NULL)
 	{
-		if (do_command(stack_a, stack_b, input) == -1)
+		if (check_params(input) == -1)
 			return (-1);
+		do_command(stack_a, stack_b, input);
+		free(input);
 		input = get_next_line(0);
-		/* 		print_stack(*stack_a);
-				print_stack(*stack_b); */
 	}
+	free(input);
 	check_is_sort(stack_a);
 	return (1);
 }
@@ -105,9 +106,10 @@ int	main(int ac, char **av)
 	if (check_stack(&stack_a, ac, av) == -1)
 		return (-1);
 	if (check_input(&stack_a, &stack_b) == -1)
-		return (-1);
-	free_stack(&stack_a);
-	free(stack_a);
-	free(stack_b);
+		return (write(2, "Error\n", 6));
+	if (stack_a)
+		free_stack(&stack_a);
+	if (stack_b)
+		free_stack(&stack_b);
 	return (0);
 }
